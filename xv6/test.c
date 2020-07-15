a993dbfa4e1b769d7d2d9f9758d52565650fce10
Edwin Leon Du <eleon029@sledge.cs.ucr.edu>
2020-07-15 12:33:17 -0700

    Update test file

--- xv6/test.c
+++ xv6/test.c
@@ -1,27 +1,23 @@
 #include "user.h"
-#include "syscall.h"
-#include "param.h"
 #include "types.h"
 #include "stat.h"
-#include "fs.h"
-#include "fcntl.h"
-#include "traps.h"
-#include "memlayout.h"
 
 int main(int argc, char *argv[]){
 	int pid;
 	int pid2;
 	int status;
-	for(int i = 0; i < 2; ++i) {
+	int i;
+	for(i = 0; i < 2; ++i) {
 		pid = fork();
 		if(pid == 0) {
-			printf(1, "This process is a child.\n");
+			printf(1, "This process is a child. PID: %d\n", pid);
 			exit(0);
 		} else if(pid > 0) {
 			pid2 = wait(&status);
-			printf(1, "This process is a parent.\n");
+			printf(1, "This process is a parent. PID: %d\n", pid2);
 		} else {
 			printf(1, "Error.\n");
+			exit(-1);
 		}
 	}
 	return 0;
