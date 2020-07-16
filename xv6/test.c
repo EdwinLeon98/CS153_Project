b998332266d1487c3cdd639681872e827e7b1712
Edwin Leon Du <eleon029@sledge.cs.ucr.edu>
2020-07-15 17:07:45 -0700

    Update test file

--- xv6/test.c
+++ xv6/test.c
@@ -1,24 +1,44 @@
 #include "types.h"
-#include "stat.h"
 #include "user.h"
 
-int main(int argc, char *argv[]){
-	int pid;
-	int pid2;
-	int status;
-	int i;
-	for(i = 0; i < 2; ++i) {
-		pid = fork();
-		if(pid == 0) {
-			printf(1, "This process is a child. PID: %d\n", pid);
-			exit(0);
-		} else if(pid > 0) {
-			pid2 = wait(&status);
-			printf(1, "This process is a parent. PID: %d\n", pid2);
-		} else {
-			printf(1, "Error.\n");
-			exit(-1);
-		}
-	}
+int exitWait(void);
+
+int main(int argc, char *argv[])
+{
+  	printf(1, "\n This program tests the correctness of the new wait and exit systemcalls\n");
+  
+	exitWait();
+	exit(0);
 	return 0;
+}
+  
+int exitWait(void) {
+	int pid, ret_pid, exit_status;
+	int i;
+	// use this part to test exit(int status) and wait(int* status)
+    
+ 	printf(1, "\n  Parts a & b) testing exit(int status) and wait(int* status):\n");
+  
+  	for (i = 0; i < 2; i++) {
+            	pid = fork();
+            	if (pid == 0) { // only the child executed this code
+                	if (i == 0){
+                        	printf(1, "\nThis is child with PID# %d and I will exit with status %d\n", getpid(), 0);
+                                exit(0);
+                        }
+                        else{
+  	                        printf(1, "\nThis is child with PID# %d and I will exit with status %d\n" ,getpid(), -1);
+                                exit(-1);
+      	                } 
+     		}
+		else if (pid > 0) { // only the parent executes this code
+         	        ret_pid = wait(&exit_status);
+                        printf(1, "\n This is the parent: child with PID# %d has exited with status %d\n", ret_pid, exit_status);
+                }
+		else {  // something went wrong with fork system call            	                                          
+                        printf(2, "\nError using fork\n");
+                        exit(-1);
+                }
+	}
+        return 0;
 }
