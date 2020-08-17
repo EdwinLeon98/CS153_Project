6102b9e8bd2a5c04a609f31e3e030a07d2cd1fa2
Edwin Leon Du <eleon029@sledge.cs.ucr.edu>
2020-08-16 20:28:28 -0700

    Moved the stack

--- lab2-f17/exec.c
+++ lab2-f17/exec.c
@@ -62,11 +62,11 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+ // sz = PGROUNDUP(sz);
+  if((sz = allocuvm(pgdir, KERNBASE-0x00000001, KERNBASE-0x00000001)) == 0)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+ // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  sp = KERNBASE-0x00000001;
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
